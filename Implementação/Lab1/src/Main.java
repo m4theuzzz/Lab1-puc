e8e98d63d6e15c96c69d2130c56b6478716b9f12
Joao Pedro Mairinque <jpmairinque@gmail.com>
2023-03-08 19:42:51 -0300

    add initial mock data

--- Implementação/Lab1/src/Main.java
+++ Implementação/Lab1/src/Main.java
@@ -1,5 +1,31 @@
+import java.util.*;
+
 public class Main {
+
+    public static List<User> generateMockUsers() {
+        List<User> mockUsers = new ArrayList<User>();
+
+        User mockSecretary1 = new Secretary("secretaria@pucmail.com", "123456");
+        User mockSecretary2 = new Secretary("secretaria@pucmail.com", "123456");
+        User mockStudent1 = new Student("estudante@pucmail.com", "123456", 1);
+        User mockStudent2 = new Student("estudante@pucmail.com", "123456", 2);
+        User mockTeacher1 = new Teacher("professor@pucmail.com", "123456");
+        User mockTeacher2 = new Teacher("professor@pucmail.com", "123456");
+
+        mockUsers.add(mockSecretary1);
+        mockUsers.add(mockSecretary2);
+        mockUsers.add(mockStudent1);
+        mockUsers.add(mockStudent2);
+        mockUsers.add(mockTeacher1);
+        mockUsers.add(mockTeacher2);
+
+        return mockUsers;
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List a = generateMockUsers();
+
+        System.out.println(a.toString());
     }
 }
